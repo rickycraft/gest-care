54ffc5019ff88796aff917913ef21127f64aed01
caterina leonelli <caterina.leonelli@gmail.com>
2022-07-27 22:30:19 +0200

    test conflict

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,5 +9,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'Caterina Leonelli' })
+  res.status(200).json({ name: 'Cose serie' })
 }
