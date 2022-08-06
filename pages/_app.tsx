32331b23a2d5aedfaa6d4717f646d6f438cdba4a
Riccardo Roveri <ricky.roveri@gmail.com>
2022-08-06 14:18:08 +0000

    added trpc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,54 @@
 //import '../styles/globals.css'
+import { withTRPC } from '@trpc/next';
+import { loggerLink } from '@trpc/client/links/loggerLink';
+import { httpBatchLink } from '@trpc/client/links/httpBatchLink';
+import superjson from 'superjson';
 import 'bootstrap/dist/css/bootstrap.css'
 import type { AppProps } from 'next/app'
+
+function getBaseUrl() {
+  if (typeof window !== 'undefined') {
+    return '';
+  }
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default withTRPC<AppRouter>({
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  config() {
+    return {
+      links: [
+        // adds pretty logs to your console in development and logs errors in production
+        loggerLink({
+          enabled: (opts) =>
+            process.env.NODE_ENV === 'development' ||
+            (opts.direction === 'down' && opts.result instanceof Error),
+        }),
+        httpBatchLink({
+          url: `${getBaseUrl()}/api/trpc`,
+        }),
+      ],
+      transformer: superjson,
+    };
+  },
+  ssr: true,
+  responseMeta(opts) {
+    const ctx = opts.ctx as SSRContext;
+    if (ctx.status) {
+      return {
+        status: ctx.status,
+      };
+    }
+    const error = opts.clientErrors[0];
+    if (error) {
+      return {
+        status: error.data?.httpStatus ?? 500,
+      };
+    }
+    return {};
+  },
+})(MyApp);
