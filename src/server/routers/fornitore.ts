09d132d5ceeb2873486a757d45de9ce05857e0af
Riccardo Roveri <ricky.roveri@gmail.com>
2022-08-09 17:20:24 +0200

    added fornitore trpc

---
+++ src/server/routers/fornitore.ts
@@ -0,0 +1,23 @@
+import { createProtectedRouter } from "server/createRouter"
+import { z } from 'zod'
+import { prisma } from 'server/prisma'
+import { TRPCError } from "@trpc/server"
+import { Prisma } from "@prisma/client"
+
+const defaultFornitoreSelect = Prisma.validator<Prisma.FornitoreSelect>()({
+  id: true,
+  nome: true,
+})
+
+export const fornitoreRouter = createProtectedRouter()
+  .query("list", {
+    input: z.any(),
+    async resolve() {
+      const fornitori = await prisma.fornitore.findMany({
+        where: {},
+        select: defaultFornitoreSelect,
+      })
+      if (!fornitori) throw new TRPCError({ code: "NOT_FOUND" })
+      return fornitori
+    }
+  })
