26280140445b2d72b172701568c9bd5bee609636
caterina leonelli <caterina.leonelli@gmail.com>
2022-08-10 15:50:03 +0200

    Update Layout. Included navbar with about, login and logout menu

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,27 +1,56 @@
-import React, { useState } from 'react'
-import useUser from '../lib/useUser'
-import Layout from '../components/Layout'
+import { useRouter } from 'next/router'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import Form from '../components/Form'
-import fetchJson, { FetchError } from '../lib/fetchJson'
+import { FetchError } from '../lib/fetchJson'
+import { InferGetServerSidePropsType } from "next";
+import { withIronSessionSsr } from 'iron-session/next'
+import { sessionOptions } from "server/iron"
+import { IronSessionData } from 'iron-session';
 
-
-export default function Login() {
+//
+export default function Login({ }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   // here we just check if user is already logged in and redirect to profile
-  const { mutateUser } = useUser({
-    redirectTo: '/', //se l'utente riesce a loggarsi, allora viene rediretto all'index
-    redirectIfFound: true,
-  })
-
-  //useState inizializza lo stato errorMsg a messaggio vuoto e utilizza come setState, il setErrorMsg
+  const [user, setUser] = useState<IronSessionData>();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    //useState inizializza lo stato errorMsg a messaggio vuoto e utilizza come setState, il setErrorMsg
   // quindi per modificare errorMsg bisogna chiamare setErrorMsg
-  const [errorMsg, setErrorMsg] = useState('') 
+  const [errorMsg, setErrorMsg] = useState('')
+  const router = useRouter()
+  console.log('[DBG LOGIN] state of isLoggedIn: ' + isLoggedIn)
+
+  const firstUpdate = useRef(true);
+  useLayoutEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+    console.log('[DBG LOGIN] isLoggedIn: ' + isLoggedIn)
+    if (isLoggedIn === false) {
+      setErrorMsg("Wrong username or password!");
+    }else{
+      setErrorMsg('')
+    }
+  }, [isLoggedIn, errorMsg]);
+
+  useEffect(() => {
+    console.log('[DBG LOGIN] isLoggedIn: ' + isLoggedIn)
+    if (isLoggedIn === true) {
+      router.push('/')
+    }
+  }, [isLoggedIn, router]);
+
+  
+  useEffect(() => {
+    console.log('[DBG LOGIN] user : ' + user)
+  }, [ user]);
+
 
   return (
-    <Layout>
+    <div>
       <div className="login">
         <Form
           errorMessage={errorMsg}
-          onSubmit={async function handleSubmit(event) {
+          onSubmit={async (event) => {
             event.preventDefault()
 
             const body = {
@@ -31,14 +60,16 @@ export default function Login() {
 
             try {
               // console.log('[DBG LOGIN] body: '+body.username+' '  + body.password)
-
-              mutateUser(
-                await fetchJson('/api/auth/login', {
-                  method: 'POST',
-                  headers: { 'Content-Type': 'application/json' },
-                  body: JSON.stringify(body),
-                })
-              )
+               const response= await fetch('/api/auth/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(body),
+              })
+                if(response.ok){
+                  let json = await response.json();
+                  console.log('response json: ' + JSON.stringify(json));
+                  setIsLoggedIn(json.isLoggedIn);
+                }
             } catch (error) {
               if (error instanceof FetchError) {
                 setErrorMsg(error.data.message)
@@ -58,7 +89,27 @@ export default function Login() {
           border-radius: 4px;
         }
       `}</style>
-    </Layout>
+
+    </div>
   )
 }
 
+export const getServerSideProps = withIronSessionSsr(async function ({
+  req,
+  res,
+}) {
+  const user = req.session.user;
+  console.log('session: '+user);
+  if (user === undefined || user.isLoggedIn===false) {
+    return {
+      props: {
+        user: { id: -1, username: '', isLoggedIn: false}, //ATT debole ma da modificare
+      },
+    };
+  }
+
+  return {
+    props: { user: req.session.user},
+  };
+},
+  sessionOptions);
