fddfeef2c86c0af82ef8806cf36c90d79282d97a
Riccardo Roveri <ricky.roveri@gmail.com>
2022-08-19 20:05:55 +0200

    fix middleware in dev

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,11 +5,13 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   const next = NextResponse.next()
-  //console.log(request.nextUrl)
+
+  if (process.env.NODE_ENV === 'development') {
+    return next
+  }
 
   const cookie = request.cookies.get("iron-cookie")
   if (cookie === undefined) return NextResponse.redirect(new URL('/login', request.nextUrl.origin))
-
   // check for role here
 
   return next
@@ -17,5 +19,5 @@ export async function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/preventivo/:page*', '/'],
+  matcher: ['/about/:page*'],
 }
