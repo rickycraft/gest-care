8bdf50756aada18e4e46aa2e64211585f3db4ec5
Riccardo Roveri <ricky.roveri@gmail.com>
2022-08-19 14:32:41 +0200

    fix merge error

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import { useRouter } from 'next/router'
 import { trpc } from 'utils/trpc'
-import { useEffect, useMemo, useState } from 'react'
+import { useMemo, useState } from 'react'
 
 const BasicMenuItem = ({ title, path }: { title: string, path: string }) => {
   return (
@@ -13,14 +13,6 @@ const BasicMenuItem = ({ title, path }: { title: string, path: string }) => {
     </Link>
   )
 }
-
-const basicMenuLinks = [
-  { title: 'Home', path: '/', },
-  { title: 'About', path: '/about', },
-  { title: 'Prodotti', path: '/prodotto', },
-  { title: 'Preventivi', path: '/preventivo', },
-  { title: 'Login', path: '/login', },
-]
 
 const basicMenuLinks = [
   { title: 'Home', path: '/', },
