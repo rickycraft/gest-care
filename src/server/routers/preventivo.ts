b957ca825c715949fa6578f6b2ee371abe206b40
Riccardo Roveri <ricky.roveri@gmail.com>
2022-08-22 21:42:38 +0200

    added all preventivo endpoints

--- src/server/routers/preventivo.ts
+++ src/server/routers/preventivo.ts
@@ -5,20 +5,30 @@ import { TRPCError } from "@trpc/server"
 import { Prisma } from "@prisma/client"
 import { rowRouter } from './preventivo_row'
 
-const defaultPrevSelect = Prisma.validator<Prisma.PreventivoSelect>()({
+const prevSelect = {
   id: true,
   nome: true,
   scuola: true,
   listino: true,
   lastEditedBy: true,
-})
+}
+const defaultPrevSelect = Prisma.validator<Prisma.PreventivoSelect>()(prevSelect)
 
-const insertPrevSchema = z.object({
+const prevSchema = {
   nome: z.string(),
   listino: z.number(),
   scuola: z.number(),
-})
+}
+
+const insertPrevSchema = z.object(prevSchema)
 export type insertPrevType = z.infer<typeof insertPrevSchema>
+
+const updatePrevSchema = z.object({
+  id: z.number(),
+  ...prevSchema,
+})
+export type updatePrevType = z.infer<typeof updatePrevSchema>
+
 
 export const prevRouter = createProtectedRouter()
   .query('byId', {
@@ -28,6 +38,10 @@ export const prevRouter = createProtectedRouter()
     resolve: async ({ input }) => {
       return await prisma.preventivo.findFirst({
         where: { id: input.id },
+        select: {
+          ...prevSelect,
+          rows: true,
+        }
       })
     }
   })
@@ -54,7 +68,39 @@ export const prevRouter = createProtectedRouter()
             listino: { connect: { id: input.listino } },
             lastEditedBy: { connect: { id: ctx.user.id } },
           },
+          select: defaultPrevSelect
+        })
+      } catch {
+        throw new TRPCError({ code: "BAD_REQUEST" })
+      }
+    }
+  })
+  .mutation('update', {
+    input: updatePrevSchema,
+    resolve: async ({ input, ctx }) => {
+      try {
+        return await prisma.preventivo.update({
+          where: { id: input.id },
+          data: {
+            nome: input.nome,
+            scuola: { connect: { id: input.scuola } },
+            listino: { connect: { id: input.listino } },
+            lastEditedBy: { connect: { id: ctx.user.id } },
+          },
           select: defaultPrevSelect,
+        })
+      } catch {
+        throw new TRPCError({ code: "BAD_REQUEST" })
+      }
+    }
+  })
+  .mutation('delete', {
+    input: z.object({ id: z.number() }),
+    resolve: async ({ input, ctx }) => {
+      try {
+        return await prisma.preventivo.delete({
+          where: { id: input.id },
+          select: {},
         })
       } catch {
         throw new TRPCError({ code: "BAD_REQUEST" })
