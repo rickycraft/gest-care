dc261e28389e8e1d3fa0f45922bba77e0fe0182f
Riccardo Roveri <ricky.roveri@gmail.com>
2022-08-24 19:07:55 +0200

    change stale time

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -47,7 +47,7 @@ export default withTRPC<AppRouter>({
       queryClientConfig: {
         defaultOptions: {
           queries: {
-            staleTime: 2 * 60 * 1000,
+            staleTime: 30 * 1000,
             cacheTime: 10 * 60 * 1000,
           }
         }
