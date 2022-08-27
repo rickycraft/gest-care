fe7e5284bbc7a895194816e8b60c52d6822b41c5
Riccardo Roveri <ricky.roveri@gmail.com>
2022-08-27 19:52:40 +0200

    user added role

--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -1,11 +1,11 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { withSessionRoute } from "server/iron";
+import { NextApiRequest, NextApiResponse } from "next"
+import { withSessionRoute } from "server/iron"
 
 
 export default withSessionRoute(
   async function logoutRoute(req: NextApiRequest, res: NextApiResponse) {
-    req.session.destroy();
-    res.send({username:'', id:-1 , isLoggedIn:false});
+    req.session.destroy()
+    res.send({ username: '', id: -1, role: '', isLoggedIn: false })
 
   },
-);
+)
--- src/utils/atom.ts
+++ src/utils/atom.ts
@@ -3,6 +3,7 @@ import { atomWithStorage } from 'jotai/utils'
 export const defaultUserAtom = {
   id: -1,
   username: '',
+  role: '',
   isLoggedIn: false,
 }
 export const userAtom = atomWithStorage('user', defaultUserAtom)
