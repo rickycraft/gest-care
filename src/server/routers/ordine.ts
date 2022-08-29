a70d22797e2a3bc1ff313433f25535d75b5f52d0
Riccardo Roveri <ricky.roveri@gmail.com>
2022-08-29 21:50:03 +0200

    ordine added id

--- src/server/routers/ordine.ts
+++ src/server/routers/ordine.ts
@@ -2,8 +2,6 @@ import { createProtectedRouter } from "server/createRouter"
 import { z } from 'zod'
 import { prisma } from 'server/prisma'
 import { TRPCError } from "@trpc/server"
-import { Prisma } from "@prisma/client"
-import { Decimal } from '@prisma/client/runtime'
 
 export const ordineRouter = createProtectedRouter()
   .query("byId", {
@@ -11,16 +9,33 @@ export const ordineRouter = createProtectedRouter()
       id: z.number(),
     }),
     async resolve({ input }) {
-      const ordine = await prisma.ordine.findFirst({ where: { id: input.id } })
+      const ordine = await prisma.ordine.findFirst({
+        where: { id: input.id },
+        include: { preventivo: true }
+      })
       if (!ordine) throw new TRPCError({ code: "BAD_REQUEST" })
-      const _rows = await prisma.preventivoRow.findMany({
+      const rows = await prisma.preventivoRow.findFirst({
         where: { preventivoId: ordine.preventivoId },
         select: {
-          OrdineRow: true
+          OrdineRow: {
+            select: {
+              id: true,
+              quantity: true,
+              prevRow: {
+                select: {
+                  provvigioneSC: true,
+                  provvigioneComm: true,
+                  provvigioneRappre: true,
+                  prodotto: true,
+                  personalizzazione: true,
+                }
+              }
+            }
+          }
         }
       })
-      const rows = _rows.map(row => row.OrdineRow)
-      return { ...ordine, rows }
+      if (!rows) throw new TRPCError({ code: "BAD_REQUEST" })
+      return { ...ordine, rows: rows.OrdineRow }
     }
   })
   .query("list", {
@@ -35,6 +50,10 @@ export const ordineRouter = createProtectedRouter()
           preventivo: {
             select: { id: true, nome: true }
           }
+        },
+        take: 10,
+        orderBy: {
+          id: "desc"
         }
       })
     }
@@ -45,10 +64,17 @@ export const ordineRouter = createProtectedRouter()
     }),
     async resolve({ input }) {
       try {
+        const rows = await prisma.preventivoRow.findMany({
+          where: { preventivoId: input.preventivoId },
+          select: { id: true }
+        })
         await prisma.ordine.create({
           data: {
             preventivo: { connect: { id: input.preventivoId } },
           }
+        })
+        await prisma.ordineRow.createMany({
+          data: rows.map(row => ({ preventivoRowId: row.id }))
         })
       } catch {
         throw new TRPCError({ code: "BAD_REQUEST" })
