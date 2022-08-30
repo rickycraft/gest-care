09c8f7649bf36843a5491cb079ff2e5957389322
Riccardo Roveri <ricky.roveri@gmail.com>
2022-08-30 20:46:12 +0200

    changed deploy script to persist container

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ fi
 docker build -t gest-care:latest . && \
   echo "Image built" && \
   docker container stop gest-care && \
+  docker container rm gest-care && \
   echo "Container stopped" && \
-  docker run -p 80:80 --rm -d --name gest-care gest-care && \
+  docker run -p 80:80 -d --name gest-care --restart "unless-stopped" gest-care && \
   echo "Container started"
