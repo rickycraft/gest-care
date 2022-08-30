1e5d7d2043b62f8e76378de2a3e7512f4a8854fa
Riccardo Roveri <ricky.roveri@gmail.com>
2022-08-30 11:34:52 +0200

    ordineList added margins

--- src/pages/ordine/list.tsx
+++ src/pages/ordine/list.tsx
@@ -13,9 +13,10 @@ export default function List() {
     <div>
       <div className='mb-3'>
         {ordineList.data.map(ordine => (
-          <Card key={ordine.id} onClick={() => router.push(`/ordine/${ordine.id}`)}>
-            <Card.Body>
-              <Card.Title>{ordine.preventivo.nome}</Card.Title>
+          <Card key={ordine.id} onClick={() => router.push(`/ordine/${ordine.id}`)} className='mb-3'>
+            <Card.Body className='d-flex justify-content-between align-items-center'>
+              <h2>{ordine.preventivo.nome.toUpperCase()}</h2>
+              <span>Altra descrizione</span>
             </Card.Body>
           </Card>
         ))}
