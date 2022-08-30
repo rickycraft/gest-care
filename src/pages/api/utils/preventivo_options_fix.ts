77733fa44fd040f3b40f20e5a290edcaed0f2827
Riccardo Roveri <ricky.roveri@gmail.com>
2022-08-30 23:20:02 +0200

    option fix optimized

--- src/pages/api/utils/preventivo_options_fix.ts
+++ src/pages/api/utils/preventivo_options_fix.ts
@@ -1,18 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from 'server/prisma'
 
-export default async function Prisma(req: NextApiRequest, res: NextApiResponse) {
+export default async function OptionsFix(req: NextApiRequest, res: NextApiResponse) {
   try {
     const opts = await prisma.preventivoDefaultOpt.findMany()
     const preventivi = await prisma.preventivo.findMany({
       select: {
         id: true,
-        options: true,
+        _count: { select: { options: true } }
       }
     })
     var counter = 0
     preventivi.forEach(async (preventivo) => {
-      if (preventivo.options.length != 0) return
+      if (preventivo._count.options != 0) return
       console.log('adding to id', preventivo.id)
       await prisma.preventivoOption.createMany({
         data: opts.map(opt => ({
