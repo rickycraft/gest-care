e9dfb5f763226eb0c5442dff16d2dbfcde0b6a80
Riccardo Roveri <ricky.roveri@gmail.com>
2022-09-18 12:00:35 +0200

    removed metrics

--- src/pages/api/metrics/prisma.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { prisma } from 'server/prisma'
-
-export default async function Prisma(req: NextApiRequest, res: NextApiResponse) {
-  const metrics = await prisma.$metrics.prometheus()
-  res.end(metrics)
-}
